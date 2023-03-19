f5cba16e6f24af856883ec19f7f6f96596a17330
Sophia Dmytrenko <dmitrenkosof@gmail.com>
2023-03-19 19:55:48 +0200

    19.03

---
+++ src/Pages/About.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Contacts extends Component {
+    render() {
+        return (
+            <div>
+                About us
+            </div>
+        );
+    }
+}
+
+export default Contacts;
+
